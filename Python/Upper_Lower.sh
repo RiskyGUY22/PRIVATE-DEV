f8c4fde1dfed1919d9a537a875928aa1d0385d97
RiskyGUY <113388138+RiskyGUY22@users.noreply.github.com>
2023-11-22 09:51:23 +0000

    blank

--- Python/Upper_Lower.sh
+++ Python/Upper_Lower.sh
@@ -20,16 +20,30 @@ data = ["Inside of DOG you can find DO", \
     "In CUPBOARD it may really surprise you to find BOAR", \
     "And in CORNER you have CORN "]
 
-for i in range(0,1):
-    
-    first_line = data[i]
+first = data[0][10:13]
+second = data[0][27:29]
 
-    print(first_line)
+fifth = data[1][10:13]
+sixth = data[1][27:29]
 
-    for j in first_line:
-        try:
-            if j.upper() == "j":
-                print(j)
-        except:
-            print("can't be lowered")
+eight = data[5][11:13]
+nine = data[5][24:29]
+
+six1 = data[6][6:9]
+six2 = data[6][20:25]
+
+eight1 = data[8][10:13]
+eight2 = data[8][24:31]
+
+nine1 = data[9][3:5]
+nine2 = data[9][9:15]
+
+ten1 = data[10][10:13]
+ten2 = data[10][24:29]
+
+print(first, second, fifth, sixth, eight, nine, six1, six2, eight1, eight2, nine1, nine2)
+
+
+
+
 
