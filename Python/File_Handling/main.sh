17b92d93db9284c073383cf8cef1322485197be6
RiskyGUY <113388138+RiskyGUY22@users.noreply.github.com>
2023-12-08 12:21:21 +0000

    test

--- Python/File_Handling/main.sh
+++ Python/File_Handling/main.sh
@@ -1,11 +1,33 @@
 #!/bin/env python
+my_list = []
+
+user_moode = input('Enter mode: ')
+
 
 try:
+
     with open('data', 'r') as f:
-        for line in f:
-            print(line)
+
+        text_files = f.readlines()
+
+        for line in text_files:
+            
+            line = line.rstrip('\n')
+            #or
+            #if '\n' in line:
+            #    line = line[:-1]
+            
+            my_list.append(line)
+            
+        print(my_list)
     
 except FileNotFoundError:
     print('File not found')
+
+    f.close()
+
+with open('data', 'a') as f:
+
+    f.write('\nThis is a new line')
 
     f.close()
---
+++ Python/File_Handling/write_to_file.sh
@@ -0,0 +1,6 @@
+#!/bin/env python
+
+wiht open('data', 'w') as f:
+    f.write('Thomas')
+
+
